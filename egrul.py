1bdabcc5b448431e3fd418f6d8ce01e2a2c3e90e
Павел Сергеев <pavel.al.sergeev@gmail.com>
2022-06-10 21:27:02 +0300

    realized recieving egrul extraction

--- egrul.py
+++ egrul.py
@@ -2,15 +2,15 @@ import datetime as dt
 import json
 import logging
 import sys
+from exceptions import EGRULException
 from http import HTTPStatus
 from logging import StreamHandler
+from typing import Callable, Dict
 
 import requests as r
 
-from exceptions import EGRULException
-
-EGRUL_ENDPOINT = "https://egrul.nalog.ru/search-result/"
-REQUEST_URL = "https://egrul.nalog.ru/"
+FTS_URL = 'https://egrul.nalog.ru/'
+SEARCH_ENDPOINT = 'search-result/'
 HEADERS = {
     "Host": "egrul.nalog.ru",
     "Connection": "Close",
@@ -27,58 +27,55 @@ logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 
-def get_company_token(query: int):
-    """Отправляет запрос с ИНН/ОГРН из инпута.
-    Возвращает словарь с токеном организации.
-    """
-    # проверка, что query это int
+def get_companys_token(query: int) -> Dict:
+    """Отправляет POST-запрос с ИНН/ОГРН.
+    Возвращает словарь с токеном организации."""
     payload = json.dumps({
         "vyp3CaptchaToken": "",
         "page": "",
         "query": query,
         "region": ""
     })
+
     try:
-        response = r.post(REQUEST_URL, data=payload, headers=HEADERS)
+        response = r.post(FTS_URL, data=payload, headers=HEADERS)
     except r.exceptions.RequestException:
-        msg = f'Невозможно отправить запрос на {REQUEST_URL}'
-        logger.error(msg)
+        msg = f'Невозможно отправить запрос на {FTS_URL}'
         raise EGRULException(msg)
 
     if response.status_code != HTTPStatus.OK:
-        msg = f'Сайт "{REQUEST_URL}" недоступен, код: {response.status_code}'
-        logger.error(msg)
+        msg = f'Сайт "{FTS_URL}" недоступен, код: {response.status_code}'
         raise EGRULException(msg)
+
     return response.json()
 
 
-def get_information(company_token: str):
+def get_company_data(company_token: str) -> Dict:
     """Отправляет GET-запрос к API и возвращает словарь с данными компании."""
-    url = EGRUL_ENDPOINT + company_token
+    url = FTS_URL + SEARCH_ENDPOINT + company_token
 
     try:
         response = r.get(url)
     except r.exceptions.RequestException:
         msg = f'Невозможно отправить запрос на {url}'
-        logger.error(msg)
         raise EGRULException(msg)
 
     try:
-        rows_list = response.json().get("rows")
+        rows_list = response.json()["rows"]
     except KeyError:
-        msg = 'Список компаний не содержит ключа "rows"'
-        logger.error(msg)
+        msg = 'Перечень компаний не содержит ключа "rows"'
         raise EGRULException(msg)
+
     try:
         company = rows_list[0]
     except IndexError:
-        msg = 'Неверный ИНН или ОГРН'
-        logger.error(msg)
+        msg = 'По этому ИНН/ОГРН ничего не нашлось. Проверьте написание.'
         raise EGRULException(msg)
+
     return company
 
 
-def chech_expire(company_dict):
+def check_expire(company_dict: Dict) -> str:
     """Проверяет исключено ли лицо из ЕГРЮЛ."""
     try:
         expire_date = company_dict["e"]
@@ -88,10 +85,16 @@ def chech_expire(company_dict):
         return f'Действует на {date}'
 
 
-def parse_individual(company_dict):
-    name = company_dict["n"].title()
-    inn = company_dict["i"]
-    ogrn = company_dict["o"]
+def parse_individual(company_dict: Dict) -> str:
+    """Получает словарь с данными компании.
+    Возвращает имя, ИНН и ОГРНИП индивидуального предпринимателя."""
+    try:
+        name = company_dict["n"].title()
+        inn = company_dict["i"]
+        ogrn = company_dict["o"]
+    except KeyError:
+        msg = 'Отсутвуют или изменены ключи с данными об ИП'
+        raise EGRULException(msg)
 
     return (
         f'ИП: {name}\n'
@@ -100,16 +103,23 @@ def parse_individual(company_dict):
     )
 
 
-def parse_company(company_dict):
-    company_name = company_dict["c"]
-    ceo = company_dict["g"].title()
-    inn = company_dict["i"]
-    ogrn = company_dict["o"]
-    kpp = company_dict["p"]
-    address = company_dict["a"].title()
+def parse_company(company_dict: Dict) -> str:
+    """Получает словарь с данными компании.
+    Возвращает название, ЕИО, ОГРН, ИНН, КПП и адрес компании."""
+    try:
+        company_name = company_dict["c"]
+        ceo = company_dict["g"].title()
+        inn = company_dict["i"]
+        ogrn = company_dict["o"]
+        kpp = company_dict["p"]
+        address = company_dict["a"].title()
+    except KeyError:
+        msg = 'Отсутвуют или изменены ключи с данными о компании'
+        raise EGRULException(msg)
 
     return (
         f'Компания: {company_name}\n'
+        '\n'
         f'{ceo}\n'
         f'ИНН: {inn}\n'
         f'ОГРН: {ogrn}\n'
@@ -118,31 +128,35 @@ def parse_company(company_dict):
     )
 
 
-def parse_information(company_dict) -> str:
-    """Получает словарь с данными компании.
-    Возвращает название, ЕИО, ОГРН, ИНН, КПП и адрес компании.
-    Либо возвращает имя, ИНН и ОГРНИП индивидуального предпринимателя.
-    """
+def parse_information(company_dict: Dict) -> Callable[[Dict], str]:
+    """Получает словарь с данными компании."""
+    # не придумал докстринг на русском
     if company_dict["k"] == "fl":
         return parse_individual(company_dict)
     return parse_company(company_dict)
 
 
-def parse_egrul(query) -> str:
-    """Получает на вход ИНН или ОГРН и возвращает информацию о компании.
-    """
+def parse_egrul(query: int) -> str:
+    """Получает на вход ИНН или ОГРН и возвращает информацию о компании."""
     try:
-        token_dict = get_company_token(query)
-        company_dict = get_information(token_dict["t"])
-        expire = chech_expire(company_dict)
+        token_dict = get_companys_token(query)
+        company_dict = get_company_data(token_dict["t"])
+        expire = check_expire(company_dict)
         inf = parse_information(company_dict)
+
     except EGRULException as error:
-        return print(f'Во время работы программы произошла ошибка: {error}')
+        logger.error(error)
+        raise Exception(f'При поиске произошла ошибка.\n{error}')
+
+    except Exception as e:
+        logger.error(e)
+        raise Exception(f'Во время работы программы произошла ошибка.\n{e}')
 
     return f'{inf}\n{expire}'
 
 
 def main():
+    print('Введите ИНН или ОГРН:')
     query = int(input())
     information = parse_egrul(query)
     return print(information)
--- egrul_vyp.py
+++ egrul_vyp.py
@@ -1,45 +1,96 @@
-import datetime as dt
-import json
 from exceptions import EGRULException
-from http import HTTPStatus
+from time import sleep
+from typing import Dict
 
 import requests as r
 
-from logger import logger as log
+from egrul import get_company_data, get_companys_token
 
-EGRUL_ENDPOINT = "https://egrul.nalog.ru/search-result/"
-REQUEST_URL = "https://egrul.nalog.ru/"
-# ссылка для скачивания выписки ЕГРЮЛ
-# нужно добавить токен из словаря с инфой о компании и что-то еще сделать(
-DOWNLOAD_URL = "https://egrul.nalog.ru/vyp-download/"
-HEADERS = {
-    "Host": "egrul.nalog.ru",
-    "Connection": "Close",
-    "Content-Type": "application/json",
-    "User-Agent": "python-requests/2.27.1"
-}
+FTS_URL = "https://egrul.nalog.ru/"
+EXCTACTION_REQUEST_ENDPOINT = 'vyp-request/'
+EXCTACTION_STATUS_ENDPOINT = 'vyp-status/'
+EXCTACTION_DOWNLOAD_ENDPOINT = "vyp-download/"
 
 
-def get_company_token(query: int):
-    """Отправляет запрос с ИНН/ОГРН из инпута.
-    Возвращает словарь с токеном организации.
-    """
-    # проверка, что query это int
-    payload = json.dumps({
-        "vyp3CaptchaToken": "",
-        "page": "",
-        "query": query,
-        "region": ""
-    })
+def request_extraction(token: str) -> Dict:
+    """Отправляет GET-запрос к API налоговой."""
+    url = FTS_URL + EXCTACTION_REQUEST_ENDPOINT + token
+
     try:
-        response = r.post(REQUEST_URL, data=payload, headers=HEADERS)
+        responce = r.get(url)
     except r.exceptions.RequestException:
-        msg = f'Невозможно отправить запрос на {REQUEST_URL}'
-        log.error(msg)
+        msg = f'Невозможно отправить запрос на {url}'
         raise EGRULException(msg)
 
-    if response.status_code != HTTPStatus.OK:
-        msg = f'Сайт "{REQUEST_URL}" недоступен, код: {response.status_code}'
-        log.error(msg)
+    return responce.json()
+
+
+def get_extraction_status(token: str) -> Dict:
+    """Отправляет API запрос на формирование выписки ЕГРЮЛ."""
+    url = FTS_URL + EXCTACTION_STATUS_ENDPOINT + token
+
+    try:
+        responce = r.get(url)
+    except r.exceptions.RequestException:
+        msg = f'Невозможно отправить запрос на {url}'
         raise EGRULException(msg)
-    return response.json()
+
+    return responce.json()
+
+
+def check_status(status_dict: Dict) -> bool:
+    """Проверяет статус готовности выписки."""
+    try:
+        status = status_dict['status']
+    except KeyError:
+        msg = 'Список компаний не содержит ключа "status"'
+        raise EGRULException(msg)
+    return status == 'ready'
+
+
+def get_download_link(status_dict: Dict, token: str) -> str:
+    """Получает ссылку для скачивания выписки."""
+    counter = 0
+    while counter != 1:
+        if not check_status(status_dict):
+            sleep(0.1)
+            counter += 1
+        return FTS_URL + EXCTACTION_DOWNLOAD_ENDPOINT + token
+    msg = 'ЕГРЮЛ не смог сформировать выписку'
+    raise EGRULException(msg)
+
+
+def get_extraction(query: int) -> str:
+    """Получает ИНН/ОГРН, возвращает ссылку на скачиание pdf."""
+    try:
+        token_dict = get_companys_token(query)
+        company_dict = get_company_data(token_dict["t"])
+
+        token = company_dict["t"]
+        request_extraction_dict = request_extraction(token)
+
+        if not token == request_extraction_dict["t"]:
+            token = request_extraction_dict["t"]
+
+        status_dict = get_extraction_status(token)
+
+        download_link = get_download_link(status_dict, token)
+
+    except EGRULException as error:
+        raise Exception(f'При формировании выписки произошла ошибка.\n{error}')
+
+    except Exception as e:
+        raise Exception(f'Во время работы программы произошла ошибка.\n{e}')
+
+    return download_link
+
+
+def main():
+    print('Введите ИНН или ОГРН:')
+    query = int(input())
+    extraction_url = get_extraction(query)
+    return print(extraction_url)
+
+
+if __name__ == '__main__':
+    main()
